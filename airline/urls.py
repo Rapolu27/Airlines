3f3027f44cba35350ad3802cec2530bcf47c7da8
priyanka <x22249346@student.ncirl.ie>
2024-07-20 14:05:33 +0100

    Airlines devops

--- airline/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('flights/', include('flights.urls')),
-    path('users/', include("users.urls")),
-    path('', include("users.urls")),
-]
