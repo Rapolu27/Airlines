3808be55a9becce88f97a64f82e05d91352dd1ab
priyanka <x22249346@student.ncirl.ie>
2024-07-20 13:48:56 +0100

    Airlines devops

--- airline/settings.py
+++ airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b6kn%^9w7#377ine1_y5xfn75^dgzg94-)8+&=e9nk0$_j#&pd'
+# SECRET_KEY = 'django-insecure-b6kn%^9w7#377ine1_y5xfn75^dgzg94-)8+&=e9nk0$_j#&pd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
